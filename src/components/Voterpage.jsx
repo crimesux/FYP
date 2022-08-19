de8cbf4c1359316a8c706e747cd137ca272c5065
crimesux <88722572+crimesux@users.noreply.github.com>
2022-08-19 16:01:29 +0800

    Delete Voterpage.jsx

--- src/components/Voterpage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom'
-import VoterListComponent from './VoterListComponent';
-
-export default function ListCampaignComponentFunctional(props) {
-    const navigate = useNavigate();
-    return <VoterListComponent {...props} navigate={navigate}/>
-}
